58b698ce7ba3a681dc58d322191f378803779e7d
shehio <shehabyasser@gmail.com>
2020-07-19 08:51:40 -0700

    More modifications on the env

--- env.py
+++ env.py
@@ -1,8 +1,7 @@
-#! /usr/bin/env python
-import time
 from PIL import Image
 import numpy as np
 import airsim
+import matplotlib.pyplot as plt
 
 
 class DroneEnv:
@@ -19,22 +18,20 @@ class DroneEnv:
     def __init__(self, duration: int = 1):
         self.duration = duration
         self.episode = 0
+
+        self.client = self.__get_client()
+        self.pose = self.client.simGetVehiclePose()
+
         self.reset()
         print(f'Initial position: ({self.state.x_val}, {self.state.y_val}, {self.state.z_val})\n')
 
     def reset(self):
         print('RESET\n\n')
-        self.client = self.__get_client()
-        self.pose = self.client.simGetVehiclePose()
-        self.state = self.client.getMultirotorState().kinematics_estimated.position
 
-        self.client.moveToPositionAsync(-10, 10, -10, 5).join()
+        self.state = self.client.getMultirotorState().kinematics_estimated.position
+        self.client.moveToPositionAsync(0, 0, -10, 5).join()
 
         self.quad_offset = (0, 0, 0)
-        self.start_collision = "Cube"
-        self.next_collision = "Cube"
-        self.cnt_collision = 0
-        self.collision_change = False
         self.episode += 1
 
     def step(self, action):
@@ -47,18 +44,13 @@ class DroneEnv:
         quad_vel = self.client.getMultirotorState().kinematics_estimated.linear_velocity
 
         print(f'Current Velocity: ({quad_vel.x_val}, {quad_vel.y_val}, {quad_vel.z_val})')
+
         self.__move_quadrotor(quad_vel)
-
-        collision_info = self.client.simGetCollisionInfo()
-        if self.next_collision != collision_info.object_name:
-            self.collision_change = True
-
-        self.__check_for_collision(collision_info)
         quad_state = self.client.getMultirotorState().kinematics_estimated.position
         quad_vel = self.client.getMultirotorState().kinematics_estimated.linear_velocity
         print(f'Position After: ({quad_state.x_val}, {quad_state.y_val}, {quad_state.z_val})\n\n')
 
-        reward = self.__compute_reward(quad_state, quad_vel, collision_info)
+        reward = self.__compute_reward(quad_state, quad_vel, self.client.simGetCollisionInfo())
         state = self.__get_observation()
         done = self.__is_done(reward)
         return state, reward, done
@@ -72,12 +64,13 @@ class DroneEnv:
         client.takeoffAsync().join()
         return client
 
+    # @Todo: Add a hover action so it stays where the desired destination is.
     def __interpret_action(self, action: int):
         step_size = 5
         if action == 0:
             self.quad_offset = (0, 0, 0)
         elif action == 1:
-            self.quad_offset = (step_size, 0, -1)
+            self.quad_offset = (step_size, 0, 0)
         elif action == 2:
             self.quad_offset = (0, step_size, 0)
         elif action == 3:
@@ -95,28 +88,17 @@ class DroneEnv:
         x_velocity = quad_vel.x_val + self.quad_offset[0]
         y_velocity = quad_vel.y_val + self.quad_offset[1]
         z_velocity = quad_vel.z_val + self.quad_offset[2]
-        self.client.simPrintLogMessage(f'Moving ({x_velocity}, {y_velocity}, {z_velocity})')
+        self.client.simPrintLogMessage(f'Moving velocity: ({x_velocity}, {y_velocity}, {z_velocity})')
         self.client.moveByVelocityAsync(x_velocity, y_velocity, z_velocity, self.duration).join()
 
-    def __check_for_collision(self, collision_info):
-        if collision_info.has_collided:
-            if self.cnt_collision == 0:
-                self.start_collision = collision_info.object_name
-                self.next_collision = collision_info.object_name
-                self.cnt_collision = 1
-            else:
-                self.next_collision = collision_info.object_name
-
     def __compute_reward(self, quad_state, quad_vel, collision_info):
-        thresh_dist, max_dist, beta, z = 7, 500, 1, -10
-
-        if self.collision_change and self.next_collision != self.start_collision and "Cube" in self.next_collision:
+        bound = 100
+        if collision_info.has_collided:
+            reward = -1000
+        elif quad_state.x_val > bound or quad_state.y_val > bound or quad_state.z_val > bound:
             reward = -1000
         else:
             reward = - self.__get_distance(quad_state)
-
-        if quad_state.z_val < -280:
-            reward = -100
 
         return reward
 
@@ -127,16 +109,20 @@ class DroneEnv:
 
     def __is_done(self, reward):
         done = 0
-        if reward <= -10:
-            done = 1
-        elif reward > 499:
+
+        # There are three ways an episode can end: Collision, Going out of bounds,
+        # and Reaching the desired destination. The first two cases are covered with reward < 1000.
+        # The third case is covered by reward being close enough (hence the -1) to the destination.
+        if reward < -100 or reward > -1:
             done = 1
 
         if done == 1:
+            # @Todo: Why are we disabling control here?
             print("======DONE======\n")
-            self.client.armDisarm(False)
-            self.client.reset()
-            self.client.enableApiControl(False)
+            self.client.simPrintLogMessage(f'Episode {self.episode} done, reward is: {reward}, resetting.')
+            # self.client.armDisarm(False)
+            self.reset()
+            # self.client.enableApiControl(False)
         return done
 
     @staticmethod
@@ -155,5 +141,8 @@ class DroneEnv:
 
         image = Image.fromarray(img2d)
         im_final = np.array(image.resize((84, 84)).convert("L"))
+
+        # Uncomment to see the sample.
+        # plt.imshow(im_final)
 
         return im_final
